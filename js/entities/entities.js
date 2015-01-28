1c08cfa8b911a1a387740158bd020ebcd3fc508f
DominicMorquecho <dominic.morquecho@gmail.com>
2015-01-28 10:45:18 -0800

    Awesomenauts: completed video 10: coded and commented. animated the player

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -13,8 +13,10 @@ game.PlayerEntity = me.Entity.extend({
 
 		this.body.setVelocity(5, 20); /*20 for the y value is essentially gravity so that the player falls*/
 
-		this.renderable.addAnimation("walk",
-		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+		this.renderable.addAnimation("idle", [78]); /*adds the animation for idle*/
+		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);/*and for walk*/
+
+		this.renderable.setCurrentAnimation("idle"); /*by default set the animation for idle*/
 
 	},
 
@@ -23,17 +25,31 @@ game.PlayerEntity = me.Entity.extend({
 			/* this adds the position of my x by adding the velocity and multiplying it by me.timer.tick*/
 			/*me.timer.tick makes the movement look smooth*/
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+			this.flipX(true);
 		}
 		else if(me.input.isKeyPressed("left")) { /*if the right arrow is being pushed then.....*/
 			/* this adds the position of my x by adding the velocity and multiplying it by me.timer.tick*/
 			/*me.timer.tick makes the movement look smooth*/
 			this.body.vel.x -= this.body.accel.x * me.timer.tick;
+			this.flipX(false);
 		}
 		else{ /*if its not being pushed then its not moving*/
 			this.body.vel.x = 0; 
 		}
+		if(this.body.vel.x !== 0) { /*if the velocity doesnt equal 0*/
+			if(!this.renderable.isCurrentAnimation("walk")) { /*and if the animation is currently not "walk"*/
+				this.renderable.setCurrentAnimation("walk"); /* then set it to walk*/
+			}
+		}
+		else {
+			this.renderable.setCurrentAnimation("idle"); /*otherwise*/
+			}
+
+
 
 		this.body.update(delta); /*delta is the change in time*/
+		
+		this._super(me.Entity, "update", [delta]);
 		return true;
 	}
 });
