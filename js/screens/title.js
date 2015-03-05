60dfe2b51bc633e6c71c18ecb2da097aadd112f1
DominicMorquecho <dominic.morquecho@gmail.com>
2015-03-05 10:45:55 -0800

    Completed coded and commented video 40

--- js/screens/title.js
+++ js/screens/title.js
@@ -30,10 +30,28 @@ game.TitleScreen = me.ScreenObject.extend({
 				me.state.change(me.state.PLAY);
 			}
 		})));
+		me.game.world.addChild (new (me.Renderable.extend({ /*adds me.Renderable*/
+			init: function() { /*initially make...*/
+				this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+				this.font = new me.Font("Comic Sans MS", 46, "pink");
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true); /*make it listen for the mouse being clicked on the screen*/
+			},
+
+			draw: function(renderer) {
+				this.font.draw(renderer.getContext(), "Continue", this.pos.x, this.pos.y); /*putting continue message*/
+			},
+
+			update: function(dt) {
+				return true;
+			},
+
+			newGame: function() { /*when a new game is started*/
+				me.input.releasePointerEvent('pointerdown', this); /*get rid of mouse click event*/
+				me.state.change(me.state.PLAY);
+			}
+		})));
 
 	},
-	
-	
 	/**	
 	 *  action to perform when leaving this screen (state change)
 	 */
