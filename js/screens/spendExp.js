f6557cda34a6b54c0818c5f3f9b23274d164204d
DominicMorquecho <dominic.morquecho@gmail.com>
2015-03-09 12:13:41 -0700

    Completed video 43

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -7,13 +7,18 @@ game.SpendExp = me.ScreenObject.extend({
 	
 		me.game.world.addChild (new (me.Renderable.extend({ /*adds me.Renderable*/
 			init: function() { /*initially make...*/
-				this._super(me.Renderable, 'init', [270, 240, 300, 50]);
+				this._super(me.Renderable, 'init', [10, 10, 300, 50]);
 				this.font = new me.Font("Comic Sans MS", 46, "pink");
 			},
 
 			draw: function(renderer) {
-				this.font.draw(renderer.getContext(), "Spend", this.pos.x, this.pos.y); /*putting start a new game message*/
-			}
+				this.font.draw(renderer.getContext(), "Press 1-5 to buy, Enter to skip", this.pos.x, this.pos.y); /*putting start a new game message*/
+				this.font.draw(renderer.getContext(), "Current Exp:" + game.data.exp.toString(), this.pos.x + 50, this.pos.y + 50);
+				this.font.draw(renderer.getContext(), "1) Make More Gold" + game.data.exp.toString(), this.pos.x + 50, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), "2) Add Starting Gold" + game.data.exp.toString(), this.pos.x + 50, this.pos.y + 150);
+				this.font.draw(renderer.getContext(), "3) Increase Damage" + game.data.exp.toString(), this.pos.x + 50, this.pos.y + 200);
+				this.font.draw(renderer.getContext(), "4) Increase Health" + game.data.exp.toString(), this.pos.x + 50, this.pos.y + 250);
+			}	
 		})));
 	
 
