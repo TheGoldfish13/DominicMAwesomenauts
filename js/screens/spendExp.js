1a996d7e9c0fad3457729bb4c90e814259f06391
DominicMorquecho <dominic.morquecho@gmail.com>
2015-03-10 10:59:33 -0700

    Completed coded and commented video 44

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -4,29 +4,59 @@ game.SpendExp = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); /*loads the title screen*/ // TODO
+
+		/*binding function keys*/
+		me.input.bindKey(me.input.KEY.F1, "F1");
+		me.input.bindKey(me.input.KEY.F2, "F2");
+		me.input.bindKey(me.input.KEY.F3, "F3");
+		me.input.bindKey(me.input.KEY.F4, "F4");
+		me.input.bindKey(me.input.KEY.F5, "F5");
 	
 		me.game.world.addChild (new (me.Renderable.extend({ /*adds me.Renderable*/
 			init: function() { /*initially make...*/
 				this._super(me.Renderable, 'init', [10, 10, 300, 50]);
-				this.font = new me.Font("Comic Sans MS", 46, "pink");
+				this.font = new me.Font("Comic Sans MS", 26, "pink");
 			},
 
 			draw: function(renderer) {
 				this.font.draw(renderer.getContext(), "Press 1-5 to buy, Enter to skip", this.pos.x, this.pos.y); /*putting start a new game message*/
-				this.font.draw(renderer.getContext(), "Current Exp:" + game.data.exp.toString(), this.pos.x + 50, this.pos.y + 50);
-				this.font.draw(renderer.getContext(), "1) Make More Gold" + game.data.exp.toString(), this.pos.x + 50, this.pos.y + 100);
-				this.font.draw(renderer.getContext(), "2) Add Starting Gold" + game.data.exp.toString(), this.pos.x + 50, this.pos.y + 150);
-				this.font.draw(renderer.getContext(), "3) Increase Damage" + game.data.exp.toString(), this.pos.x + 50, this.pos.y + 200);
-				this.font.draw(renderer.getContext(), "4) Increase Health" + game.data.exp.toString(), this.pos.x + 50, this.pos.y + 250);
+				this.font.draw(renderer.getContext(), "Current Exp:", this.pos.x + 50, this.pos.y + 50);
+				this.font.draw(renderer.getContext(), "1) Make More Gold Current Level: " + game.data.exp1.toString() + " Cost:" + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), "2) Add Starting Gold", this.pos.x, this.pos.y + 150);
+				this.font.draw(renderer.getContext(), "3) Increase Damage", this.pos.x, this.pos.y + 200);
+				this.font.draw(renderer.getContext(), "4) Increase Health", this.pos.x, this.pos.y + 250);
+				this.font.draw(renderer.getContext(), "5) Skip", this.pos.x, this.pos.y + 300);
 			}	
 		})));
-	
+
+	this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
+		if(action === "F1") {
+
+		}
+		else if(action === "F2") {
+
+		}
+		else if(action === "F3") {
+
+		}
+		else if(action === "F4") {
+			
+		}
+		else if(action === "F5") { /*if F5 is pressed then play the game*/
+			me.state.change(me.state.PLAY);
+		}
+	});
 
 	},
 	/**	
 	 *  action to perform when leaving this screen (state change)
 	 */
-	onDestroyEvent: function() {
-
+	onDestroyEvent: function() { /*unbind all the keys*/
+		me.input.unbindKey(me.input.KEY.F1, "F1");
+		me.input.unbindKey(me.input.KEY.F2, "F2");
+		me.input.unbindKey(me.input.KEY.F3, "F3");
+		me.input.unbindKey(me.input.KEY.F4, "F4");
+		me.input.unbindKey(me.input.KEY.F5, "F5");
+		me.event.unsubscribe(this.handler);
 	}
 });
