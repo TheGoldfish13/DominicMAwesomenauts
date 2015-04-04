dc645009db3895fb0a641e352042f4d23e636684
DominicMorquecho <dominic.morquecho@gmail.com>
2015-04-04 13:31:26 -0700

    Coded Completed and Commented video 56

--- php/controller/create-db.php
+++ php/controller/create-db.php
@@ -1,20 +1,5 @@
 <?php
 	require_once(__DIR__ . "/../model/config.php"); //takes code from config and connects to server
- 
-/*test comment for source tree*/
-	$query = $_SESSION["connection"]->query("CREATE TABLE posts (" /*creates a table called posts that needs....*/
-		. "id int(11) NOT NULL AUTO_INCREMENT," /*needs a id that will be an integer, cant be empty*/
-		. "title varchar (255) NOT NULL," /*needsa title up to 255 characters and cant be nothing*/
-		. "post text NOT NULL,"  /*the actual text of the post, Cant be empty*/
-		. "DateTime datetime NOT NULL," /*inserts Datetime in the table that cannot be*/
-		. "PRIMARY KEY (id) )"); /*the tables primary key is the id*/
-
-if($query) {
-		echo " Succesfully created table: posts"; //runs the query, creates the table once
-	}
-	// else {
-	// 	echo "<p>" . $_SESSION["connection"]->error . "</p>"; //echos out that the table has already been created
-	// }
 
 	$query = $_SESSION["connection"]->query("CREATE TABLE users (" /*creates a table that contains the information of username, email, and password*/
 		. "id int(11) NOT NULL AUTO_INCREMENT,"
@@ -22,6 +7,11 @@ if($query) {
 		. "email varchar(50) NOT NULL,"
 		. "password varchar(128) NOT NULL,"
 		. "salt char(128) NOT NULL,"
+		. "exp int(4)," /*cut off integer of exp at 4 so at 9999*/
+		. "exp1 int(4),"
+		. "exp2 int(4),"
+		. "exp3 int(4),"
+		. "exp4 int(4),"
 		. "PRIMARY KEY (id))");
 	if($query) {
 		echo "<p>Succesfully created table user: </p>"; /*checking if table user was succefully created*/
--- php/controller/save-user.php
+++ php/controller/save-user.php
@@ -1,2 +1,23 @@
 <?php
-	
+	require_once(__DIR__ . "/../model/config.php");
+
+	$exp = filter_input(INPUT_POST, "exp", FILTER_SANITIZE_STRING); /*have 5 experience variables*/
+	$exp1 = filter_input(INPUT_POST, "exp", FILTER_SANITIZE_STRING);
+	$exp2 = filter_input(INPUT_POST, "exp", FILTER_SANITIZE_STRING);
+	$exp3 = filter_input(INPUT_POST, "exp", FILTER_SANITIZE_STRING);
+	$exp4 = filter_input(INPUT_POST, "exp", FILTER_SANITIZE_STRING);
+
+
+	$query = $_SESSION["connection"]->query("UPDATE users SET " /*store the 5 exp variables as exp where the user is logged in*/
+		. "exp = $exp, "
+		. "exp1 = $exp1, "
+		. "exp2 = $exp2, "
+		. "exp3 = $exp3, "
+		. "exp4 = $exp4 WHERE username = \"" . $_SESSION["name"]. "\"");
+
+	if($query){ /*if the query works*/
+		echo "true"; /*echo true (for ajax later)*/
+	}
+	else{ /*if its broke*/
+		echo "<p>" . $_SESSION["connection"]->error . "</p>"; /*then echo why*/
+	}
